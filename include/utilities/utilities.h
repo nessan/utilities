7ee86c07b683e568ee395b12212d6b56945a4449
nessan <nessan.fitzmaurice@me.com>
2025-12-15 14:54:18 +0000

    excluded fomatter.h from the include most things  header file (ambiguous  `std::formatter` specialisations)

--- include/utilities/utilities.h
+++ include/utilities/utilities.h
@@ -6,7 +6,6 @@
 /// An "include everything" header for the utilities library.
 
 #include <utilities/confirm.h>
-#include <utilities/formatter.h>
 #include <utilities/log.h>
 #include <utilities/macros.h>
 #include <utilities/stopwatch.h>
@@ -14,3 +13,7 @@
 #include <utilities/string.h>
 #include <utilities/thousands.h>
 #include <utilities/type.h>
+
+// However, we do not include formatter.h here -- it can give rise to  ]ambiguous `std::formatter` specialisations.
+// You can include it directly if you need it and are aware of the issues.
+// #include <utilities/formatter.h>
